9a4e4bb4d126f97395753abba35363bc7fd62d2d
SUDEEP PENNA <SUDEEP PENNA@DESKTOP-6CA878U.alphionsee.in>
2022-10-22 21:11:52 +0530

    First commit through Eclipse

---
+++ GitHubEclipseDemo1/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.pages;
+
+public class LoginPage {
+	public void logigPage() {
+		System.out.println("This is Login Page");
+			
+	}
+
+
+}
---
+++ GitHubEclipseDemo1/src/test/java/com/testCases/LoginPageTest.java
@@ -0,0 +1,9 @@
+package com.testCases;
+
+public class LoginPageTest {
+	
+	public void logigPageTest() {
+		System.out.println("This is Login Page Test");
+	}
+
+}
